34337b41bf33c1041e3130294251e207954ed8e5
Dmitry_Z <mmeto*@gmail.com>
2022-11-23 12:52:51 +0200

    Start project

---
+++ src/PetProject.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Main Class to do smth
+ */
+public class PetProject {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        //int a = sc.nextInt();
+        System.out.println( "Hello?" );
+    }
+}
