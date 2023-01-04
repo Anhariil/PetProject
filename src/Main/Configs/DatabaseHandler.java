48d7b2606fa415bcba00bd9b88963278c21ea8e4
Anhariil <mmetor9991@gmail.com>
2023-01-04 22:30:38 +0200

    DROP-18 fix DB connection, work wth collect data

--- src/Main/Configs/DatabaseHandler.java
+++ src/Main/Configs/DatabaseHandler.java
@@ -3,6 +3,8 @@ package Main.Configs;
 import Main.Connectors.Data.Tinkoff.InsrumentService.Instrument;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseHandler extends Configs {
     Connection dbConnection;
@@ -23,8 +25,8 @@ public class DatabaseHandler extends Configs {
     }
 
     public void saveShares(Instrument instruments) throws ClassNotFoundException {
-        String insert = "INSERT INTO " + Const.SHARE_TABLE + "(" + Const.SHARE_FIGI + "," + Const.SHARE_COUNTRY_OF_RISK
-                + "," + Const.SHARE_COUNTRY_OF_RISK_NAME + "," + Const.SHARE_SECTOR + ")" + "VALUES(?,?,?,?)";
+        String insert = "INSERT INTO " + InstrumentConst.SHARE_TABLE + "(" + InstrumentConst.SHARE_FIGI + "," + InstrumentConst.SHARE_COUNTRY_OF_RISK
+                + "," + InstrumentConst.SHARE_COUNTRY_OF_RISK_NAME + "," + InstrumentConst.SHARE_SECTOR + "," + InstrumentConst.SHARE_NAME + ")" + "VALUES(?,?,?,?,?)";
 
         try {
             PreparedStatement prSt = getDbConnection().prepareStatement(insert); // set our request
@@ -33,6 +35,8 @@ public class DatabaseHandler extends Configs {
             prSt.setString(2, instruments.getCountryOfRisk());
             prSt.setString(3, instruments.getCountryOfRiskName());
             prSt.setString(4, instruments.getSector());
+            prSt.setString(5, instruments.getName());
+
 
             prSt.executeUpdate(); // to set info in DB
         } catch (SQLException e) {
@@ -40,11 +44,80 @@ public class DatabaseHandler extends Configs {
         }
     }
 
+    public void updateInstruments(Instrument[] instruments) {
+        for (Instrument i : instruments) {
+            updateInstrument(i);
+        }
+    }
+
+    public void updateInstrument(Instrument instrument) {
+        String update = "UPDATE " + InstrumentConst.SHARE_TABLE + " SET " +
+                InstrumentConst.SHARE_TICKER + " =? , " + InstrumentConst.SHARE_CLASSCODE + " =? , " +
+                InstrumentConst.SHARE_ISIN + " =? , " + InstrumentConst.SHARE_LOT + " =? , " +
+                InstrumentConst.SHARE_CURRENCY + " =? , " + InstrumentConst.SHARE_SHORT_ENABLED_FLAG + " =? , " +
+                InstrumentConst.SHARE_NAME + " =? , " + InstrumentConst.SHARE_EXCHANGE + " =? , " +
+                InstrumentConst.SHARE_IPO_DATE + " =? , " + InstrumentConst.SHARE_ISSUE_SIZE + " =? , " +
+                InstrumentConst.SHARE_COUNTRY_OF_RISK + " =? , " + InstrumentConst.SHARE_COUNTRY_OF_RISK_NAME + " =? , " +
+                InstrumentConst.SHARE_SECTOR + " =? , " + InstrumentConst.SHARE_ISSUE_SIZE_PLAN + " =? , " +
+                InstrumentConst.SHARE_TRADING_STATUS + " =? , " +
+                InstrumentConst.SHARE_OTC_FLAG + " =? , " + InstrumentConst.SHARE_BUY_AVAILABLE_FLAG + " =? , " +
+                InstrumentConst.SHARE_SELL_AVAILABLE_FLAG + " =? , " + InstrumentConst.SHARE_DIV_YIELD_FLAG + " =? , " +
+                InstrumentConst.SHARE_SHARE_TYPE + " =? , " + InstrumentConst.SHARE_API_TRADE_AVAILABLE_FLAG + " =? , " +
+                InstrumentConst.SHARE_UID + " =? , " + InstrumentConst.SHARE_REAL_EXCHANGE + " =? , " +
+                InstrumentConst.SHARE_POSITION_UID + " =? , " + InstrumentConst.SHARE_FOR_IIS_FLAG + " =? , " +
+                InstrumentConst.SHARE_FOR_QUAL_INVESTOR_FLAG + " =? , " + InstrumentConst.SHARE_WEEKEND_FLAG + " =? , " +
+                InstrumentConst.SHARE_BLOCKED_TCA_FLAG + " =? , " + InstrumentConst.SHARE_FIRST_MIN_CANDLE + " =? , " +
+                InstrumentConst.SHARE_FIRST_DAY_CANDLE + " =? " + " WHERE " + InstrumentConst.SHARE_FIGI + " = ?";
+        try {
+            PreparedStatement prSt = getDbConnection().prepareStatement(update); // set our request
+            // set our values
+            prSt.setString(1, instrument.getTicker());
+            prSt.setString(2, instrument.getClassCode());
+            prSt.setString(3, instrument.getIsin());
+            prSt.setInt(4, instrument.getLot());
+            prSt.setString(5, instrument.getCurrency());
+            prSt.setBoolean(6, instrument.isShortEnabledFlag());
+            prSt.setString(7, instrument.getName());
+            prSt.setString(8, instrument.getExchange());
+            prSt.setString(9, instrument.getIpoDate());
+            prSt.setString(10, instrument.getIssueSize());
+            prSt.setString(11, instrument.getCountryOfRisk());
+            prSt.setString(12, instrument.getCountryOfRiskName());
+            prSt.setString(13, instrument.getSector());
+            prSt.setString(14, instrument.getIssueSizePlan());
+            prSt.setString(15, instrument.getTradingStatus());
+            prSt.setBoolean(16, instrument.isOtcFlag());
+            prSt.setBoolean(17, instrument.isBuyAvailableFlag());
+            prSt.setBoolean(18, instrument.isSellAvailableFlag());
+            prSt.setBoolean(19, instrument.isDivYieldFlag());
+            prSt.setString(20, instrument.getShareType());
+            prSt.setBoolean(21, instrument.isApiTradeAvailable());
+            prSt.setString(22, instrument.getUid());
+            prSt.setString(23, instrument.getRealExchange());
+            prSt.setString(24, instrument.getPositionUid());
+            prSt.setBoolean(25, instrument.isForIisFlag());
+            prSt.setBoolean(26, instrument.isForQualInvestorFlag());
+            prSt.setBoolean(27, instrument.isWeekendFlag());
+            prSt.setBoolean(28, instrument.isBlockedTcaFlag());
+            prSt.setString(29, instrument.getFirst1minCandleDate());
+            prSt.setString(30, instrument.getFirst1dayCandleDate());
+            prSt.setString(31, instrument.getFigi());
+
+            prSt.executeUpdate(); // to set info in DB
+        } catch (SQLException e) {
+            System.out.println("Error in updateInstrument: " + e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public String[] getNamesByCountryOfRisk(String country) {
         ResultSet rs = null;
         String[] resultString = null;
-        String select = "SELECT " + Const.SHARE_COUNTRY_OF_RISK + " FROM " + Const.SHARE_TABLE + " WHERE "
-                + Const.SHARE_COUNTRY_OF_RISK + "=?";
+        List<String> stringList = new ArrayList<>();
+
+        String select = "SELECT " + InstrumentConst.SHARE_NAME + " FROM " + InstrumentConst.SHARE_TABLE + " WHERE "
+                + InstrumentConst.SHARE_COUNTRY_OF_RISK + "=?";
         try {
             PreparedStatement prSt = getDbConnection().prepareStatement(select); // set our request
             prSt.setString(1, country);
@@ -53,18 +126,18 @@ public class DatabaseHandler extends Configs {
             System.out.println("Error in getNamesByCountryOfRisk: " + e);
         }
 
-        int counter = 0;
-
         try {
-            while (rs.next()) {
-                counter++;
+            while (rs.next()) { // for each string
+                stringList.add(rs.getString(1)); // we take first string in line, cos it's our country of risk
             }
         } catch (SQLException e) {
             System.out.println("Error in getNamesByCountryOfRisk: " + e);
         }
 
-        if (counter > 0) {
-
+        // if we have results - fill it in array
+        if (stringList.size() > 0) {
+            resultString = new String[stringList.size()];
+            stringList.toArray(resultString);
         }
 
         return resultString;
---
+++ src/Main/Configs/InstrumentConst.java
@@ -0,0 +1,37 @@
+package Main.Configs;
+
+public class InstrumentConst {
+    public static final String SHARE_TABLE = "instruments";
+    public static final String SHARE_FIGI = "figi";
+    public static final String SHARE_TICKER = "ticker";
+    public static final String SHARE_CLASSCODE = "classCode";
+    public static final String SHARE_ISIN = "isin";
+    public static final String SHARE_LOT = "lot";
+    public static final String SHARE_CURRENCY = "currency";
+    public static final String SHARE_SHORT_ENABLED_FLAG = "shortEnabledFlag";
+    public static final String SHARE_NAME = "name";
+    public static final String SHARE_EXCHANGE = "exchange";
+    public static final String SHARE_IPO_DATE = "ipoDate";
+    public static final String SHARE_ISSUE_SIZE = "issueSize";
+    public static final String SHARE_COUNTRY_OF_RISK = "countryOfRisk";
+    public static final String SHARE_COUNTRY_OF_RISK_NAME = "countryOfRiskName";
+    public static final String SHARE_SECTOR = "sector";
+    public static final String SHARE_ISSUE_SIZE_PLAN = "issueSizePlan";
+    public static final String SHARE_TRADING_STATUS = "tradingStatus";
+    public static final String SHARE_OTC_FLAG = "otcFlag";
+    public static final String SHARE_BUY_AVAILABLE_FLAG = "buyAvailableFlag";
+    public static final String SHARE_SELL_AVAILABLE_FLAG = "sellAvailableFlag";
+    public static final String SHARE_DIV_YIELD_FLAG = "divYieldFlag";
+    public static final String SHARE_SHARE_TYPE = "shareType";
+    //    public static final String SHARE_MIN_PRICE_INCREMENT = "minPriceIncrement"; // id to another table
+    public static final String SHARE_API_TRADE_AVAILABLE_FLAG = "apiTradeAvailableFlag";
+    public static final String SHARE_UID = "uid";
+    public static final String SHARE_REAL_EXCHANGE = "realExchange";
+    public static final String SHARE_POSITION_UID = "positionUid";
+    public static final String SHARE_FOR_IIS_FLAG = "forIisFlag";
+    public static final String SHARE_FOR_QUAL_INVESTOR_FLAG = "forQualInvestorFlag";
+    public static final String SHARE_WEEKEND_FLAG = "weekendFlag";
+    public static final String SHARE_BLOCKED_TCA_FLAG = "blockedTcaFlag";
+    public static final String SHARE_FIRST_MIN_CANDLE = "first1minCandleDate";
+    public static final String SHARE_FIRST_DAY_CANDLE = "first1dayCandleDate";
+}
